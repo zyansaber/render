c941994073a9690466f8c6693eba1a51700b8ec8
zyansaber <zyan112491@gmail.com>
2025-06-03 22:30:33 +1000

    Delete source_package directory

--- source_package/src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-# Create a single SQLAlchemy instance to be used across the application
-db = SQLAlchemy()
-
-# Do not import models here to avoid circular imports
-# Import models directly from their respective files in other modules
