4f900866778c7a5e24f39a58373905cb483679e7
zyansaber <zyan112491@gmail.com>
2025-06-11 09:51:04 +1000

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -13,12 +13,16 @@ class User(db.Model):
     is_active = db.Column(db.Boolean, default=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
     def __repr__(self):
         return f'<User {self.username}>'
+
+    @property
+    def accessible_pages(self):
+        return [perm.page for perm in self.page_permissions]
